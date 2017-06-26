c01e6023883f3e0821967835f9c2779255579c51
LizRuelas <liz.ruelas.borda@gmail.com>
2017-06-26 16:02:12 -0500

    agregando log

--- web/index.php
+++ web/index.php
@@ -1,3 +1,21 @@
 <?php
-echo 'Hola mundo'
+  // Retrieve the request's body and parse it as JSON
+  $input = @file_get_contents("php://input");
+  $event_json = json_decode($input);
+
+  // Do something with $event_json
+
+
+
+
+  // Escribir en archivo
+  $myfile = fopen("log/log-webhooks.txt", "w") or die("Imposible abrir el archivo.");
+
+
+  fwrite($myfile, $input);
+  fclose($myfile);
+
+
+  // Respuesta a Culqi, todo bien.
+  http_response_code(200); // PHP 5.4 o superior
 ?>
