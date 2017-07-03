3f8c76f1bef17fdd80770f269b24e08f637c367f
LizRuelas <liz.ruelas.borda@gmail.com>
2017-07-03 16:33:17 -0500

    test

--- web/index.php
+++ web/index.php
@@ -1,13 +1,13 @@
 <?php
   // Retrieve the request's body and parse it as JSON
-  $input = @file_get_contents("php://input");
+  $input = file_get_contents("php://input");
   $event_json = json_decode($input);
 
   // Do something with $event_json
 
 
   // Escribir en archivo
-  $myfile = fopen("log/log-webhooks.json", "w") or die("Imposible abrir el archivo.");
+  $myfile = fopen("log/log-webhooks.txt", "w") or die("Imposible abrir el archivo.");
 
 
   fwrite($myfile, $input);
