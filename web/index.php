0aaa6ecae824dc8de0264b43aea83ec9e04ec57d
LizRuelas <liz.ruelas.borda@gmail.com>
2017-09-26 16:45:11 -0500

    update_php

--- web/index.php
+++ web/index.php
@@ -4,14 +4,13 @@
   $event_json = json_decode($input);
 
 
-  // Escribir en archivo
+  // Escribir el Webhook en mi archivo
   $myfile = fopen("log/log-webhooks.json", "w") or die("Imposible abrir el archivo.");
-
+  fwrite($myfile, $input);
 
   //Respuesta a Culqi
   $array = array(
     "mensaje_response" => "Objeto token recibido"
   );
-
   echo json_encode($array);
 ?>
